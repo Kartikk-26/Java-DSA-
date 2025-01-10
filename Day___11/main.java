1b052a0f647f852dbd5d546f4f3d81745f821d50
Kartikjain2626 <kartikjain2626@gmail.com>
2025-01-10 11:51:38 +0530

    Day 11

---
+++ Day___11/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main( String[] args){
+        int [] nums = {22,10,8,17,15,18};
+        int target = 8;
+        int ans = linearSearch(nums,target);
+        if (ans != -1) {
+            System.out.println("Target element found at index: " + ans);
+        } else {
+            System.out.println("Target element not found.");
+        }
+    }
+
+    // Helper Function 
+    static int linearSearch(int[] arr , int target){
+            if(arr.length==0){
+                return -1;
+            }
+
+            for(int i=0; i<arr.length; i++){
+                if(arr[i]==target){
+                    return i;
+                }
+            }
+            return -1;
+        }
+}
